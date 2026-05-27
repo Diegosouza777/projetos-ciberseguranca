65bf7f66a462be6e0d103232b0d2f4e26b930fa7
Diego Geraldo Brito Souza <diego03geraldo@gmail.com>
2026-05-27 15:15:30 -0300

    feat: corrige variável de IP e adiciona captura de banner

--- scanner-portas/scanner de portas.py
+++ scanner-portas/scanner de portas.py
@@ -1,14 +1,24 @@
 import socket
 
-ip = input("digite o endereço ip -")
+alvo= input("digite o endereço ip do alvo -")
 
 for porta in range (1, 1025):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
 
-    resultado = s.connect_ex((ip, porta))
+    resultado = s.connect_ex((alvo, porta))
 
     if resultado == 0:
+
         print (f"Porta {porta} aberta")
 
-    s.close
+        try:
+            s.send(b"HEAD / HTTP/1.0\n\r\n\r")
+            banner = s.recv(1024)
+
+            print(f"Banner: {banner.decode().strip()}")
+
+        except:
+            print("banner não encontrado")
+
+s.close
